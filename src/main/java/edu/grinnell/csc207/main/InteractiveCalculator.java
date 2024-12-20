b14d6640db2f2e899a24b977743ddc54c45a07c5
jenifer <silvajen@grinnell.edu>
2024-12-20 13:02:53 -0600

    continue to work later

--- src/main/java/edu/grinnell/csc207/main/InteractiveCalculator.java
+++ src/main/java/edu/grinnell/csc207/main/InteractiveCalculator.java
@@ -1,5 +1,10 @@
 package edu.grinnell.csc207.main;
 
+import java.io.PrintWriter;
+import javax.imageio.plugins.tiff.BaselineTIFFTagSet;
+import edu.grinnell.csc207.util.BFCalculator;
+import edu.grinnell.csc207.util.BFRegisterSet;
+import edu.grinnell.csc207.util.BigFraction;
 
 /**
  * Main method that will take expressions from the command line and print out results
@@ -8,5 +13,64 @@ package edu.grinnell.csc207.main;
  * @author Jenifer Silva
  */
 public class InteractiveCalculator {
-  
-}
+  public static void main(String[] args) {
+
+    PrintWriter pen = new PrintWriter(System.out, true);
+    BFRegisterSet reg = new BFRegisterSet();
+    BFCalculator calculator = new BFCalculator();
+
+    boolean STORE = false;
+    boolean add = false;
+    boolean subtract = false;
+    boolean divide = false;
+    boolean multiply = false;
+    BigFraction firstNum;
+    BigFraction secondNum;
+
+    for (int i = 0; i < args.length; i++) {
+      if (args[0].equals("STORE")) {
+        char register = args[1].charAt(6);
+        reg.store(register, calculator.get());
+        
+      } else if (args[i].equals("+")) {
+        add = true;
+        calculator.add(firstNum);
+
+      } else if (args[i].equals("-")) {
+        subtract = true;
+      } else if (args[i].equals("/")) {
+        divide = true;
+      } else if (args[i].equals("*")) {
+        multiply = true;
+      } else {
+        return;
+      }
+    } // if
+
+
+
+    if (STORE) {
+      
+    } // if
+
+    if (caesar && decode) {
+      pen.println(CipherUtils.caesarDecrypt(word, caesarKey));
+    } // if
+
+    if (vigenere && encode) {
+      String result;
+      result = CipherUtils.vigenereEncrypt(word, key);
+      if (result != null && !result.isEmpty()) {
+        pen.println(result);
+      } // if
+    } // if
+
+    if (vigenere && decode) {
+      String result;
+      result = CipherUtils.vigenereDecrypt(word, key);
+      if (result != null && !result.isEmpty()) {
+        pen.println(result);
+      } // if
+    } // if
+  } // main
+} // Cipher
--- src/main/java/edu/grinnell/csc207/main/QuickCalculator.java
+++ src/main/java/edu/grinnell/csc207/main/QuickCalculator.java
@@ -1,4 +1,5 @@
 package edu.grinnell.csc207.main;
+
 import java.io.PrintWriter;
 import edu.grinnell.csc207.util.BFCalculator;
 import edu.grinnell.csc207.util.BFRegisterSet;
@@ -7,7 +8,7 @@ import edu.grinnell.csc207.util.BigFraction;
 
 /**
  * This is the quick calculator main java file.
- *  
+ * 
  * @author Jenifer Silva
  * @author Samuel A. Rebelsky
  * 
@@ -15,41 +16,24 @@ import edu.grinnell.csc207.util.BigFraction;
 public class QuickCalculator {
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
-    if (args.length >= 2) {
-      System.err.println("Error:  not enough arguments");
-      pen.close();
-      return;
-    } // This if is to make sure I am only given 2 parameters for this to run
-
-    String firstNum = args[0];
-    String secondNum = args[2];
+    BigFraction firstNum = new BigFraction(args[0]);
+    BigFraction secondNum = new BigFraction(args[2]);
     String operation = args[1];
+    BFCalculator calculator = new BFCalculator();
+    calculator.add(firstNum);
 
-   switch(operation) {
-    case "+":
-      BFCalculator.add()
-   }
-
-    
-
-
-
-
-    String encoOrDeco = args[0];
-    String str = args[1];
-    if (encoOrDeco.equals("encode")) {
-      for (char ch = 'a'; ch <= 'z'; ch++) {
-        pen.printf("n = %c: %s\n", ch, CipherUtils.caesarEncrypt(str, ch));
-      } // for
-    } else if (encoOrDeco.equals("decode")) {
-      for (char ch = 'a'; ch <= 'z'; ch++) {
-        pen.printf("n = %c: %s\n", ch, CipherUtils.caesarDecrypt(str, ch));
-      } // for
+    if (operation.equals("+")) {
+      calculator.add(secondNum);
+    } else if (operation.equals("-")) {
+      calculator.subtract(secondNum);
+    } else if (operation.equals("*")) {
+      calculator.multiply(secondNum);
     } else {
-      System.err.println(
-          "Error: Invalid option: " + encoOrDeco + " Valid options are \"encode\" or \"decode\".");
-    } // if the user doesn't input encode or decode when running the code then we will give the user
-      // an error
+      calculator.divide(secondNum);
+    }
+
+    pen.println(calculator.get());
     pen.close();
+
   } // main
-} // Allcaesar
+} // quickcalculator
